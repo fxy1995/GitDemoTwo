d31ebe5e9f4ce5a876cd6fcd886a85101e962fca
fxy1995 <1460793107@qq.com>
2023-04-06 14:15:12 +0800

    master修改了TestWork中的代码

---
+++ testGitModeOne/src/com/duing/domain/TestWork.java
@@ -0,0 +1,15 @@
+package com.duing.domain;
+
+/**
+ * @Author 付雪缘
+ * @Date 2023/4/6 13:30
+ * @Version 1.0
+ */
+public class TestWork {
+    public void tsetVoid(){
+        System.out.println("hehe");
+
+        //我是Master分支添加的
+        System.out.println("helloBranchMaster");
+    }
+}
