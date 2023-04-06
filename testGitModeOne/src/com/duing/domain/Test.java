d6a2dd86292ddfa433cd73562b75ab612ac27c5e
fxy1995 <1460793107@qq.com>
2023-04-06 15:02:55 +0800

    HHH

--- testGitModeOne/src/com/duing/domain/Test.java
+++ testGitModeOne/src/com/duing/domain/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("哈啊哈哈");
+        System.out.println("hellohello666666");
     }
 }
