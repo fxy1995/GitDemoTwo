6250caaff59872cd797d1683c201b5b88fdea29d
fxy1995 <1460793107@qq.com>
2023-04-06 14:25:34 +0800

    master2次修改代码

--- testGitModeOne/src/com/duing/domain/TestWork.java
+++ testGitModeOne/src/com/duing/domain/TestWork.java
@@ -13,5 +13,9 @@ public class TestWork {
         System.out.println("helloBranchMaster");
         //我是branchB添加的
         System.out.println("helloBranchB");
+
+        //我是Master分支添加的
+        System.out.println("helloBranchMaster");
+
     }
 }
